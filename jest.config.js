0e49955ab5a7ee60a519ae7ee83a0c34a95963c5
Ian-Hodges <ian.hodges@bjss.com>
2022-01-26 16:25:10 +0000

    chore(cb2-2334): amend jest.config.js so integration tests don't run in vscode-jest extension

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/src", "<rootDir>/tests"],
-  testMatch: ["**/*.*Test.ts"],
+  testMatch: ["**/*.unitTest.ts"],
   setupFiles: ["jest-plugin-context/setup"],
   moduleFileExtensions: ["js", "ts"],
   coverageDirectory: "./coverage",
